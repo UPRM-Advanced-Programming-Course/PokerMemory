0fe780f79d00f32588b89ec30e1426db9c01d4aa
bvelez <Bienvenido.Velez@upr.edu>
2017-09-18 12:47:46 -0400

    Fixed makeDeck method to fill grid with pairs of random cards

--- PokerMemory/src/EqualPairLevel.java
+++ PokerMemory/src/EqualPairLevel.java
@@ -7,6 +7,9 @@
  * @version Sept 2017
  */
 
+import java.util.Random;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class EqualPairLevel extends EasyLevel {
@@ -14,6 +17,48 @@ public class EqualPairLevel extends EasyLevel {
 	protected EqualPairLevel(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) {
 		super(validTurnTime, mainFrame);
 		super.getTurnsTakenCounter().setDifficultyModeLabel("Medium Level");
+	}
+	
+	@Override
+	protected void makeDeck() {
+		// Creates a deck to fill the grid.  Each card appears twice in random places.
+		ImageIcon backIcon = this.getCardIcons()[this.getTotalCardsPerDeck()];
+
+		// make an array of card numbers: 0, 0, 1, 1, 2, 2, ..., 7, 7
+		// duplicate the image in as many cards as the input imageClones
+		int totalCardsInGrid = getRowsPerGrid() * getCardsPerRow();
+		int totalUniqueCards = totalCardsInGrid/2;
+
+		// Generate one distinct random card number for each unique card	
+		int cardsToAdd[] = new int[totalCardsInGrid];
+		boolean cardChosen[] = new boolean[getTotalCardsPerDeck()];
+
+		int chosenCount = 0;
+		Random rand = new Random();
+		while (chosenCount < totalUniqueCards)
+		{
+			int nextCardNo = rand.nextInt(getTotalCardsPerDeck());
+			if (!cardChosen[nextCardNo]) {
+				cardChosen[nextCardNo] = true;
+				cardsToAdd[2*chosenCount] = nextCardNo;
+				cardsToAdd[2*chosenCount + 1] = nextCardNo;
+				chosenCount++;
+			}
+		}
+
+		// randomize the order of the cards
+		this.randomizeIntArray(cardsToAdd);
+
+		// make each card object and add it to the game grid
+		for(int i = 0; i < cardsToAdd.length; i++)
+		{
+			// number of the card, randomized
+			int num = cardsToAdd[i];
+			// make the card object and add it to the panel
+			String rank = cardNames[num].substring(0, 1);
+			String suit = cardNames[num].substring(1, 2);
+			this.getGrid().add( new Card(this, this.getCardIcons()[num], backIcon, num, rank, suit));
+		}
 	}
 
 	@Override
